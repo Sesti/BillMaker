b8927b4e12eefeebb259adc706e49e870953d962
Sesti <31969390+Sesti@users.noreply.github.com>
2018-05-15 22:28:13 -0400

    Settings the foundation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BillMaker</title>
+</head>
+<body>
+<?php
+    define("ICON_DIR", "/assets/icons/");
+?>
+<h1>Bill Maker</h1>
+<img src="<?= ICON_DIR."add_entry.svg" ?>" alt="Add job entry">
+</body>
+</html>
