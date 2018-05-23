908a2087945c612026ff8388b12081d839d6f900
S Pilon <sebastian.pilon@gmail.com>
2018-05-22 21:51:31 -0400

    Now able to save data to csv

--- config.php
+++ config.php
@@ -2,3 +2,6 @@
 
 	define( "ICON_DIR", "/assets/icons/" );
 	define( "APP_TITLE", "Bill Maker");
+	define( 'ROOT_PATH', __DIR__ );
+	
+	require ROOT_PATH.'/vendor/league/csv/autoload.php';
--- controller/AddEntrySubmit.php
+++ controller/AddEntrySubmit.php
@@ -1,5 +1,10 @@
 <?php
 require_once('classes/ReturnObject.php');
+require_once('../config.php');
+
+use League\Csv\Exception;
+use League\Csv\Reader;
+use League\Csv\Writer;
 
 $return = new ReturnObject();
 
@@ -7,7 +12,12 @@ if( isset($_POST['e_token']) && $_POST['e_token'] == 1){
 	$return->setStatus(200);
 	$return->setMessage( "Successfully saved !" );
 	
+	try{
+		$csv = Writer::createFromPath( ROOT_PATH.'/bin/entries.csv', 'a+');
+		$csv->insertOne([$_POST['e_date'], $_POST['e_time'], $_POST['e_client'], $_POST['e_description']]);
+	}catch ( Exception | RuntimeException $e){
 	
+	}
 	
 	
 }else{
